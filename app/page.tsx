c9d91841b13e0461130b198e0db2bdbdc568093c
changwoo Jeong <jcw9828@gmail.com>
2023-12-07 19:09:19 +0900

    fix: build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-import Image from 'next/image'
 import React from 'react'
-import Form from './Form'
-import { ProductDto } from './types/products/products.dto'
 
 export default async function Home() {
-  const products: ProductDto.Response[] = await fetch(
-    `http://localhost:3000/api/products`,
-  ).then((res) => res.json())
-
-  return <Form products={products} />
+  return <div>test</div>
 }
