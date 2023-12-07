9fedc479e07454dc719a1435506c27ddaa73c54e
changwoo Jeong <jcw9828@gmail.com>
2023-12-07 21:12:26 +0900

    refresh db

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -66,7 +66,7 @@ export default function Page() {
   )
   return (
     <Container>
-      <p>안녕하세요. {session?.user?.name}님</p>
+      <p>안녕하세요. {session?.user?.name}님.</p>
       <Input
         style={{ marginBottom: 16 }}
         placeholder="Search"
